a8da7c04486fefdfd3c0153c026b73732d114241
Summeli <summeli@summeli.fi>
2012-12-09 23:14:51 +0200

    minor update

--- translations/Blubbels_en_GB.ts
+++ translations/Blubbels_en_GB.ts
@@ -31,7 +31,7 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/game.cpp" line="497"/>
+        <location filename="../src/game.cpp" line="498"/>
         <source>Score: %1</source>
         <translation type="unfinished"></translation>
     </message>
@@ -85,26 +85,26 @@
     </message>
     <message>
         <location filename="../src/main.cpp" line="136"/>
-        <source>&lt;center&gt;&lt;h1&gt;Blubbels 1.0&lt;/h1&gt;A Jawbreaker&amp;trade; clone written in Qt 4&lt;p&gt;The original version of Blubbels can be found on &lt;a href=&quot;http://sourceforge.net/projects/blubbels/&quot;&gt;http://sourceforge.net/projects/blubbels/&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;The BB10 version of Blubbels can be found on &lt;a href=&quot;http://github.com/Summeli/Blubbels/&quot;&gt;http://github.com/Summeli/Blubbels/&lt;/a&gt;.&lt;/p&gt;&lt;small&gt;&lt;p&gt;&amp;copy;2007-2010 Christian Pulvermacher &amp;lt;pulvermacher@gmx.de&amp;gt;&lt;br&gt;German Translation: &amp;copy;2009 Dominic Hopf &amp;lt;dh@dmaphy.de&amp;gt;&lt;/p&gt;&lt;p&gt;BB10 port: Antti Pohjola &amp;lt;summeli@summeli.fi&amp;gt;&lt;/p&gt;&lt;/small&gt;&lt;/center&gt;%1</source>
+        <source>&lt;center&gt;&lt;h1&gt;Blubbels 1.0&lt;/h1&gt;A Jawbreaker&amp;trade; clone written in Qt 4&lt;p&gt;The BB10 source code of Blubbels can be found on &lt;a href=&quot;http://github.com/Summeli/Blubbels/&quot;&gt;http://github.com/Summeli/Blubbels/&lt;/a&gt;.&lt;/p&gt;&lt;small&gt;&lt;p&gt;&amp;copy;2007-2010 Christian Pulvermacher &amp;lt;pulvermacher@gmx.de&amp;gt;&lt;br&gt;German Translation: &amp;copy;2009 Dominic Hopf &amp;lt;dh@dmaphy.de&amp;gt;&lt;/p&gt;&lt;p&gt;BB10 port: Antti Pohjola &amp;lt;summeli@summeli.fi&amp;gt;&lt;/p&gt;&lt;/small&gt;&lt;/center&gt;%1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="155"/>
+        <location filename="../src/main.cpp" line="154"/>
         <source>How to Play</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="156"/>
+        <location filename="../src/main.cpp" line="155"/>
         <source>&lt;b&gt;How to play Blubbels&lt;/b&gt;&lt;br&gt;&lt;p&gt;In the main window you can see lots of colored bubbles, arranged in &apos;stacks&apos;.&lt;/p&gt;&lt;p&gt;If there are two or more bubbles of the same color next to each other (either horizontally or vertically) they can be removed. You can see the points that will be awarded for doing so by hovering your mouse cursor over the bubbles.&lt;/p&gt;&lt;p&gt;Remove the group of bubbles by clicking on them. Any bubbles that rest above these will then fall down.  After completely emptying a stack, a new stack of bubbles of random height will be moved in from the left.&lt;/p&gt;&lt;p&gt;The game ends if there are no bubbles left on the gameboard that can be removed.&lt;/p&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="183"/>
+        <location filename="../src/main.cpp" line="182"/>
         <source>Statistics</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="184"/>
+        <location filename="../src/main.cpp" line="183"/>
         <source>&lt;table&gt;&lt;tr&gt;&lt;td&gt;Average:&lt;/td&gt;&lt;td align=&apos;right&apos;&gt;%1&lt;/td&gt;&lt;/tr&gt;&lt;tr&gt;&lt;td&gt;Highscore:&lt;/td&gt;&lt;td align=&apos;right&apos;&gt;%2&lt;/td&gt;&lt;/tr&gt;&lt;tr&gt;&lt;td&gt;Games played:&lt;/td&gt;&lt;td align=&apos;right&apos;&gt;%3&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;</source>
         <translation type="unfinished"></translation>
     </message>
